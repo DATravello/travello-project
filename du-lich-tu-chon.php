78d0e00065f09f012df011e7bf125975390e6b7c
Trieu Cao Duy <trieucaoduy98@gmail.com>
2020-06-25 19:25:05 +0700

    dat tour tu chon

--- du-lich-tu-chon.php
+++ du-lich-tu-chon.php
@@ -1,8 +1,10 @@
 <?php
 include('include/header.php');
 
-$query = "SELECT * FROM diemden";
+$query = "SELECT * FROM vitri";
 $result = mysqli_query($connection, $query);
+
+
 ?>
 
 <!-- NỘI DUNG -->
@@ -15,10 +17,13 @@ $result = mysqli_query($connection, $query);
         while ($rows = mysqli_fetch_array($result)) {
         ?>
             <div class="col-md-3">
-                <div class="card">
-                    <img class="card-img-top" src="admin/img/diem-den/<?php echo $rows["Anh"]; ?>" alt="Card image cap">
-                    <h5 class="title"><?php echo $rows["TenDiemDen"]; ?></h5>
-                </div>
+                <a href="khach-san-theo-diem-den.php?diem-den=<?php echo $rows["MaViTri"]; ?>">
+                    <div class="card">
+                        <img class="card-img-top" src="admin/img/diem-den/<?php echo $rows["Anh"]; ?>" alt="Card image cap">
+                        <h5 class="title"><?php echo $rows["TenViTri"]; ?></h5>
+                    </div>
+                </a>
+
             </div>
         <?php
         }
---
+++ khach-san-theo-diem-den.php
@@ -0,0 +1,63 @@
+<?php
+include('include/header.php');
+
+
+if (isset($_GET['diem-den'])) {
+    $madiemdien = $_GET['diem-den'];
+    require_once('database/db_config.php');
+
+    // Querry Khách sạn
+    $query = "SELECT * FROM khachsan WHERE MaViTri='$madiemdien'";
+    $result = mysqli_query($connection, $query);
+
+    // Querry Điểm đến
+    $q_diemden = "SELECT * FROM vitri WHERE MaViTri='$madiemdien'";
+    $rs_diemden = mysqli_query($connection, $q_diemden);
+    $rw_dd = mysqli_fetch_array($rs_diemden);
+
+    // $maloaiphong = 1;
+    // $q_loaiphong = "SELECT * FROM loaiphong WHERE MaLoaiPhong = '1'";
+    // $rw_lp = mysqli_fetch_array($q_loaiphong);
+}
+?>
+
+<!-- NỘI DUNG -->
+
+<section class="container hotel-content">
+
+    <h5 class="title-hotel">DANH SÁCH KHÁCH SẠN</h5>
+    <div class="row">
+        <?php
+        while ($rows = mysqli_fetch_array($result)) {
+        ?>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-img-top">
+                        <img style="height: 200px;width: 100%" src="admin/img/khach-san/<?php echo $rows['Anh'] ?>" alt="Card image cap">
+                        <div class="feature">Đang Giảm Giá</div>
+                        <div class="like"><i class="fas fa-heart"></i></div>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-location"><i class="fas fa-map-marker-alt"></i> <?php echo $rw_dd['TenViTri'] ?></p>
+                        <h5 class="card-title"><a href="dat-tour-tu-chon.php?khach-san=<?php echo $rows['MaKS']; ?>"><?php echo $rows['TenKS'] ?></a></h5>
+                        <p class="card-text">
+                            <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i> <span class="reviews">4 Reviews</span>
+                        </p>
+                    </div>
+                    <div class="card-footer d-flex">
+                        <div class="card-f-left"><i class="far fa-clock"></i> <?php echo $rows['SoPhong'] ?> Phòng</div>
+                        <div class="ml-auto card-f-right"><i class="fas fa-dollar-sign"></i> <span class="price"><?php echo product_price(550000)//($rw_lp['Gia']) ?></span></div>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+</section>
+
+
+
+<?php
+include('include/footer.php');
+?>
