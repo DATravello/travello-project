18d6e193857de972e6ad63583b026f365f889432
trieucaoduy <trieucaoduy98@gmail.com>
2020-07-27 13:45:04 +0700

    update

--- include/scripts.php
+++ include/scripts.php
@@ -6,3 +6,36 @@
 <script src="plugins/parallax-js-master/parallax.min.js"></script>
 <script src="scripts/cus.js"></script>
 <script src="scripts/sweetalert.min.js"></script>
+
+<script>
+  var div = document.createElement('div');
+  div.className = 'fb-customerchat';
+  div.setAttribute('page_id', '112630380533253');
+  div.setAttribute('ref', 'b64:dHJhdmVsbG8=');
+  document.body.appendChild(div);
+  window.fbMessengerPlugins = window.fbMessengerPlugins || {
+    init: function () {
+      FB.init({
+        appId            : '1678638095724206',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v3.3'
+      });
+    }, callable: []
+  };
+  window.fbAsyncInit = window.fbAsyncInit || function () {
+    window.fbMessengerPlugins.callable.forEach(function (item) { item(); });
+    window.fbMessengerPlugins.init();
+  };
+  setTimeout(function () {
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }, 0);
+</script>
+
